901ca07350a3888c45df5ca5257940354a41f2cc
finalblast <connor@Nicks-iMac.local>
2015-09-07 09:22:16 +0700

    Initial Commit

---
+++ NickDemoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NickDemoApp
+//
+//  Created by Nam (Nick) N. HUYNH on 9/7/15.
+//  Copyright (c) 2015 Nam (Nick) N. HUYNH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NickDemoApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NickDemoApp
+//
+//  Created by Nam (Nick) N. HUYNH on 9/7/15.
+//  Copyright (c) 2015 Nam (Nick) N. HUYNH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
